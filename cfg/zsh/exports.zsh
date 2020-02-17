ecf3b19d4d053de03dd5c8c04b101527051ac881
Clement Labbe <clabbe@Clements-MacBook-Pro.local>
2020-02-17 15:55:08 +1100

    Default to saml AWS profile

--- cfg/zsh/exports.zsh
+++ cfg/zsh/exports.zsh
@@ -13,6 +13,7 @@ export PATH="/usr/local/opt/python@3.8/bin:$PATH"
 # AWS
 export AWS_CLI=/usr/local/bin/aws
 export AWS_DEFAULT_REGION=ap-southeast-2
+export AWS_PROFILE=saml
 
 # fzf
 export FZF_DEFAULT_COMMAND='fd --type f'
