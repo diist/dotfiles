1f35e7f2ef4d624625ad6c8b918e538992a1d088
Clement Labbe <clabbe@slack-corp.com>
2020-08-03 10:38:16 +1000

    Add gitconfig

--- bin/post-brew.sh
+++ bin/post-brew.sh
@@ -8,3 +8,5 @@ chsh -s /bin/zsh
 # Disable mouse acceleration (default: 1.5)
 defaults write .GlobalPreferences com.apple.mouse.scaling -1
 
+# Imperatively configuring git to avoid overwriting .gitconfig
+git config --global rebase.autosquash true
